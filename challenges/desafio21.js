dc3b3fb9a237a7fe49b43073be76c65e2dbcd9f9
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-08-27 17:23:01 -0300

    Adiciona soluções

---
+++ challenges/desafio21.js
@@ -0,0 +1,4 @@
+db.voos.find(
+  { litrosCombustivel: { $gte: 1000 } },
+  { _id: false, vooId: true }
+).limit(1);
---
+++ challenges/desafio24.js
@@ -0,0 +1,11 @@
+db.voos.find(
+  {
+    $nor: [
+      { litrosCombustivel: { $gt: 600 } },
+      { "empresa.nome": "GOL" },
+      { "empresa.nome": "AZUL" }
+    ],
+    litrosCombustivel: { $exists: true }
+  },
+  { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }
+).limit(1);

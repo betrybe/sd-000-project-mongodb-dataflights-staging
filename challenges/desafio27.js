fe64b4919a984f951fce26848540d421ec109019
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-08-28 12:05:56 -0300

    Volta com as soluções

---
+++ challenges/desafio27.js
@@ -0,0 +1,5 @@
+db.voos.count({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" });
+
+db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos: 4187 });
+
+db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 }).limit(1);
